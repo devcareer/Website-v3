6e134b837985178eaebe03ffc6920e5a69ae5a79
PreciousAma <preshchilaka06@gmail.com>
2023-05-31 01:01:16 +0100

    ci: add theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+	ThemeProvider,
+	CssBaseline,
+} from '@mui/material';
 import App from './App';
 import './index.css';
+import { theme } from './theme';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+      <CssBaseline />
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>
 );
