ca3bc68059cd734639c68d693b4225102c14781c
Riddy105 <olaniranolatubosun@gmail.com>
2023-06-01 09:51:34 +0100

    fix: updated kuda image import

--- src/assets/Images/index.js
+++ src/assets/Images/index.js
@@ -1,14 +1,13 @@
 export { default as Logo } from './Logo.svg';
 export { default as hero } from './hero-section.png';
 export { default as isams } from './isams.png';
-export { default as kuda } from './kuda.png';
+export { default as kuda } from './Kuda.png';
 export { default as peerigon } from './peerigon.png';
 export { default as revivn } from './revivn.png';
 export { default as eden } from './eden.png';
 export { default as gofundme } from './GoFundMe.png';
-export {default as facebook} from './facebook.png'
-export {default as github} from './github.svg'
-export {default as instagram} from './instagram.svg'
-export {default as linkedin} from './linkedin.png'
-export {default as twitter} from './twitter.svg'
-
+export { default as facebook } from './facebook.png';
+export { default as github } from './github.svg';
+export { default as instagram } from './instagram.svg';
+export { default as linkedin } from './linkedin.png';
+export { default as twitter } from './twitter.svg';
