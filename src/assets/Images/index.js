b3ae3656c9c478e28d88d7727873769022e17f7e
kingjamesx <talk2james.uj@gmail.com>
2023-06-01 13:43:00 +0100

    fix: fix conflict

--- src/assets/Images/index.js
+++ src/assets/Images/index.js
@@ -1,10 +1,5 @@
 export { default as Logo } from './Logo.svg';
 export { default as hero } from './hero-section.png';
-export {default as facebook} from './facebook.png'
-export {default as github} from './github.svg'
-export {default as instagram} from './instagram.svg'
-export {default as linkedin} from './linkedin.png'
-export {default as twitter} from './twitter.svg'
 export {default as briefcase} from './briefcase.svg'
 export {default as magicpen} from './magicpen.svg'
 export {default as ranking} from './ranking.svg'
