526834343e87b9e58a598d4210ad5c9ea3e3cdc0
Riddy105 <olaniranolatubosun@gmail.com>
2023-06-02 13:30:04 +0100

    fix: clashing imports

--- src/assets/Images/index.js
+++ src/assets/Images/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export { default as Logo } from './Logo.svg';
 export { default as hero } from './hero-section.png';
 export { default as briefcase } from './briefcase.svg';
@@ -13,8 +12,6 @@ export { default as kuda } from './Kuda.png';
 export { default as peerigon } from './peerigon.png';
 export { default as revivn } from './revivn.png';
 export { default as eden } from './eden.png';
-=======
->>>>>>> 32b6036a99d4ae02778968738f68b48ec3d00556
 export { default as gofundme } from './GoFundMe.png';
 export { default as kuda } from './Kuda.png';
 export { default as Logo } from './Logo.svg';
@@ -39,15 +36,12 @@ export { default as search } from './search-normal.svg';
 export { default as talentLinkedin } from './talent-linkedin.png';
 export { default as talentWeb } from './talent-web.png';
 export { default as twitter } from './twitter.svg';
-export {default as dpd} from './dpd-pattern.png'
-export {default as timer} from './timer.svg'
-export {default as cpu} from './cpu-charge.svg'
-export {default as product} from './data.svg'
-export {default as bmagic} from './bigmagicpen.svg'
-export {default as uk} from './uk.png'
-export {default as dpdlogo}from './dpdlogo.svg'
-export {default as ukhub} from './ukhub.png'
-export {default as devcareerhub} from './devcareerhub.png'
-
-
-
+export { default as dpd } from './dpd-pattern.png';
+export { default as timer } from './timer.svg';
+export { default as cpu } from './cpu-charge.svg';
+export { default as product } from './data.svg';
+export { default as bmagic } from './bigmagicpen.svg';
+export { default as uk } from './uk.png';
+export { default as dpdlogo } from './dpdlogo.svg';
+export { default as ukhub } from './ukhub.png';
+export { default as devcareerhub } from './devcareerhub.png';
