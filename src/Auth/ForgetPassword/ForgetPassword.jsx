7fca12e8e8efd76aac055d6efe460cc1007528fa
kingjamesx <talk2james.uj@gmail.com>
2023-07-14 22:16:01 +0100

    feat: add stash files

--- src/Auth/ForgetPassword/ForgetPassword.jsx
+++ src/Auth/ForgetPassword/ForgetPassword.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
-import { Link, useNavigate, useSearchParams } from 'react-router-dom';
-import { Typography, Box, Button, Stack } from '@mui/material';
-import { Input } from '../../components';
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+import { Box, Button, Stack, Typography } from '@mui/material';
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { AuthCard } from '../../Auth';
-
+import { Input } from '../../components';
 const ForgetPassword = () => {
   const navigate = useNavigate();
-
+  const [email, setEmail] = useState('');
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleClick = async () => {
+    try {
+      const response = await axios.post(
+        'https://website-v3-znmt.onrender.com/api/v1/auth/signup',
+        {
+          email,
+          username: 'sarah200',
+          password: '12345',
+          confirmPassword: '12345',
+        }
+      );
+      const data = await response.data;
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <AuthCard>
       <Box
@@ -30,12 +50,18 @@ const ForgetPassword = () => {
         </Typography>
       </Box>
       <Stack direction="column" sx={{ gap: '24px', mt: '64px' }}>
-        <Input title="Email Address" placeholder="Enter your email address" />
+        <Input
+          title="Email Address"
+          placeholder="Enter your email address"
+          value={email}
+          onChange={handleChange}
+        />
         <Button
           variant="contained"
           color="primary"
           fontSize="20px"
           sx={{ py: '24px', color: '#FEFEFE' }}
+          onClick={handleClick}
         >
           Reset Password
         </Button>
@@ -55,7 +81,9 @@ const ForgetPassword = () => {
             variant="body1"
             color="initial"
             fontWeight="bold"
-            onClick={() => navigate('?mode=signup')}
+            onClick={() => {
+              navigate('?mode=signup');
+            }}
             sx={{
               cursor: 'pointer',
             }}
