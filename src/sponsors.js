255818290c57a5d7bf5b604f02e9906f06922608
Riddy105 <olaniranolatubosun@gmail.com>
2024-03-22 12:46:11 +0100

    ci: datacamp

--- src/sponsors.js
+++ src/sponsors.js
@@ -1,79 +1,91 @@
-import { Wennovation,Creationhub,Swahilipot,Kuda,Eden } from "./assets/Images";
-export const  partners = [
-    { src: Wennovation, alt: 'wennovation' },
-    { src: Creationhub, alt: 'creation hub' },
-    { src: Swahilipot, alt: 'swahilipot' },
-    { src: Kuda, alt: 'kuda' },
-    {
-      src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074852/ventures_q6vpxh.svg',
-      alt: 'ventures',
-    },
-    {
-      src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074852/Opolo_gbotlp.png',
-      alt: 'opolo',
-    },
-    {
-      src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074852/The_nest_lo084d.svg',
-      alt: 'the nest',
-    },
-    {
-      src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074852/The_cans_mogjuq.png',
-      alt: 'the cans',
-    },
-    {
-      src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074852/roothub-logo-1_rhgrkl.png',
-      alt: 'roothub',
-    },
-    {
-      src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074851/olotusq-logo-black-128_aajce5.png',
-      alt: 'olotu square',
-    },
-    {
-      src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074851/jacaranda_lzd8gh.webp',
-      alt: 'jacaranda',
-    },
-    {
-      src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074851/Group-10_vtr2ok.png',
-      alt: 'group 10',
-    },
-    {
-      src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074849/720logo_iunnyp.png',
-      alt: 'logo 1',
-    },
-    {
-      src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074851/Ghana_Innovation_Hub_t8y97a.png',
-      alt: 'ghana innovation hub',
-    },
-    {
-      src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074850/doja_yjqqof.svg',
-      alt: 'doja',
-    },
-    {
-      src: Eden,
-      alt: 'eden',
-      maxWidth: '150px',
-    },
-   
-    {
-      src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074849/632ac9aada179418cfc68fc3_logo_1_d0rtm0.svg',
-      alt: '632ac9aada179418cfc68fc3_logo_1',
-    },
-    {
-      src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074849/62545220a86c7d6b63383de0_PledgesLogo_gakghu.svg',
-      alt: 'pledges logo',
-    },
-    {
-      src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074850/deimos_v52dq9.png',
-      alt: 'deimos',
-    },
-   
-    {
-      src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074851/Educative_Logo_zdg9gr.jpg',
-      alt: 'educative',
-    },
-    {
-      src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074850/bongohive-512x512_rbt5h9.png',
-      alt: 'bongohive',
-      maxWidth: '150px',
-    },
-  ];
+import {
+  Wennovation,
+  Creationhub,
+  Swahilipot,
+  Kuda,
+  Eden,
+  DataCampLogo,
+} from './assets/Images';
+export const partners = [
+  {
+    src: DataCampLogo,
+    alt: 'DataCamp',
+    href: 'https://www.datacamp.com/donates?utm_source=donates&utm_medium=partner&utm_campaign=000000_1-internal_2-na_3-global_4-na_5-na_6-requirements_7-do_8-ptnr-pn_9-na_10-na_11-logo',
+  },
+  { src: Wennovation, alt: 'wennovation' },
+  { src: Creationhub, alt: 'creation hub' },
+  { src: Swahilipot, alt: 'swahilipot' },
+  { src: Kuda, alt: 'kuda' },
+  {
+    src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074852/ventures_q6vpxh.svg',
+    alt: 'ventures',
+  },
+  {
+    src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074852/Opolo_gbotlp.png',
+    alt: 'opolo',
+  },
+  {
+    src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074852/The_nest_lo084d.svg',
+    alt: 'the nest',
+  },
+  {
+    src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074852/The_cans_mogjuq.png',
+    alt: 'the cans',
+  },
+  {
+    src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074852/roothub-logo-1_rhgrkl.png',
+    alt: 'roothub',
+  },
+  {
+    src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074851/olotusq-logo-black-128_aajce5.png',
+    alt: 'olotu square',
+  },
+  {
+    src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074851/jacaranda_lzd8gh.webp',
+    alt: 'jacaranda',
+  },
+  {
+    src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074851/Group-10_vtr2ok.png',
+    alt: 'group 10',
+  },
+  {
+    src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074849/720logo_iunnyp.png',
+    alt: 'logo 1',
+  },
+  {
+    src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074851/Ghana_Innovation_Hub_t8y97a.png',
+    alt: 'ghana innovation hub',
+  },
+  {
+    src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074850/doja_yjqqof.svg',
+    alt: 'doja',
+  },
+  {
+    src: Eden,
+    alt: 'eden',
+    maxWidth: '150px',
+  },
+
+  {
+    src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074849/632ac9aada179418cfc68fc3_logo_1_d0rtm0.svg',
+    alt: '632ac9aada179418cfc68fc3_logo_1',
+  },
+  {
+    src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074849/62545220a86c7d6b63383de0_PledgesLogo_gakghu.svg',
+    alt: 'pledges logo',
+  },
+  {
+    src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074850/deimos_v52dq9.png',
+    alt: 'deimos',
+  },
+
+  {
+    src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074851/Educative_Logo_zdg9gr.jpg',
+    alt: 'educative',
+  },
+  {
+    src: 'https://res.cloudinary.com/okoyecb/image/upload/v1688074850/bongohive-512x512_rbt5h9.png',
+    alt: 'bongohive',
+    maxWidth: '150px',
+  },
+];
